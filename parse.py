f006d4ac3063ccdaa507db6947cab4fec2f0b8da
Bob Zhou <bobzhou@Bobs-MacBook-Pro.local>
2015-03-25 16:30:23 +0800

    fix format

--- parse.py
+++ parse.py
@@ -7,7 +7,7 @@ if len(sys.argv) >= 3:
 	minDate = time.strptime(sys.argv[3], "%Y-%m-%d")  if len(sys.argv) == 4 else 0
 
 	f = open(filename, 'r')
-	fo = open(time.strftime("%Y%m%d%H%M%.sql"), 'w')
+	fo = open(time.strftime("%Y%m%d%H%M.sql"), 'w')
 	text = f.read()
 	result = re.finditer("(\d{4}-\d{2}-\d{2} \d+:\d+:\d+ \wM) (.+?)\((\d+)\)", text)
 	fo.write("INSERT IGNORE INTO message_log (time, name, qq, qqgroup) VALUES \n")
@@ -25,8 +25,8 @@ if len(sys.argv) >= 3:
 	f.close()
 
 else:
-	print ("""
+	print """
 	Usage: 3 arguments: import file location which is dumped from QQ as a txt, and the qq group (such as reading or lang), then optionally, the start date to import
 	Like: python parse.py test.txt lang 2015-01-01
 	And you will get a sql with the current timestamp then upload it and wait for the task to complete
-	""")
+	"""
